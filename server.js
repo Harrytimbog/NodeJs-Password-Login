26b6c27b8d9ff7d0aef0216bb5472cb39f640d21
Timilehin Arigbede <Harriemannie@gmail.com>
2021-10-24 15:21:47 +0100

    Create the login routes

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 const express = require('express')
-
 const app = express()
+
+app.use(express.json())
+
+// I should use a real DB but I am just testing right now
+const users = []
+
+app.get('/users', (req, res) => {
+  res.json(users)
+})
+
+app.post('/users', (req, res) => {
+  const user = { name: req.body.name, password: req.body.password }
+  users.push(user)
+  res.status(201).send()
+})
 
 app.listen(3000)
